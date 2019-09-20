6bd80631b996f971bb3833bc9fe50794feaf1ba9
xabertum <jmohatar@emergya.com>
2019-09-20 10:56:57 +0200

    Added repo class

---
+++ src/main/java/com/xabertum/model/ViniloRepo.java
@@ -0,0 +1,12 @@
+package com.xabertum.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ViniloRepo extends CrudRepository<Vinilo, String> {
+	
+	List<Vinilo> findByTituloVinilo(String tituloVinilo);
+	Vinilo findByYear(String fechaEdicion);
+
+}
