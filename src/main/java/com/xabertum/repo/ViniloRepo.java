bc6b178bf34e14762db3b36647ba70faa69fa62d
xabertum <jmohatar@emergya.com>
2019-10-29 13:25:51 +0100

    minor changes

--- src/main/java/com/xabertum/repo/ViniloRepo.java
+++ src/main/java/com/xabertum/repo/ViniloRepo.java
@@ -3,10 +3,11 @@ package com.xabertum.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 import com.xabertum.model.Vinilo;
 
-public interface ViniloRepo extends JpaRepository<Vinilo, String> {
+public interface ViniloRepo extends JpaRepository<Vinilo, String>, QueryByExampleExecutor<Vinilo> {
 	
 	List<Vinilo> findByTituloVinilo(String tituloVinilo);
 	List<Vinilo> findByFechaEdicion(String fechaEdicion);
